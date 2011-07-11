640fc024e72c773b2a1f41478074f7c09c22f5e5
Toby Smith <toby@tismith.id.au>
2011-07-11 21:41:49 +1000

    Failing attempt at getting map_sum to go

--- list.cpp
+++ list.cpp
@@ -3,8 +3,13 @@
 
 #define myList LIST< 1, LIST< 2, LIST< 3, EmptyList > > >
 
+template <int n> struct SQUARE {
+    static const int VALUE = n*n;
+};
+
 int main(int argc, char **argv) {
     std::cout << "head = " << myList::HEAD << std::endl;
     std::cout << "sum = " << myList::SUM << std::endl;
     std::cout << "minimum = " << myList::MINIMUM << std::endl;
+    std::cout << "map_sum = " << MAP_SUM< myList, SQUARE >::RESULT << std::endl;
 }
--- list.hpp
+++ list.hpp
@@ -15,4 +15,19 @@ template< int a > struct LIST< a, EmptyList > {
     static const int MINIMUM = a;
 };
 
+template< class L, template <int> class F > 
+struct MAP_SUM {
+    static const int RESULT = 0;
+};
+
+template< class L, template <int> class F > 
+struct MAP_SUM< LIST< int A, typename TAIL >, F > {
+    static const int RESULT = F< A >::VALUE + MAP_SUM< TAIL, F >::RESULT;
+};
+
+template< template <int> class F > 
+struct MAP_SUM< EmptyList, F > {
+    static const int RESULT = 0;
+};
+
 #endif
