bf6da6d704467442197c95b079da176cfa7a929e
Toby Smith <toby@tismith.id.au>
2011-07-12 07:44:52 +1000

    Add a few more map-reduce examples

--- list.cpp
+++ list.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include "list.hpp"
 
-#define myList LIST< 1, LIST< 2, LIST< 3, EmptyList > > >
+#define myList LIST< 1, LIST< 2, LIST< 3, LIST <0, EmptyList > > > >
 
 template <int n> struct SQUARE {
     static const int VALUE = n*n;
+};
+
+template <int n> struct ID {
+    static const int VALUE = n;
 };
 
 template <int a, int b> struct ADDITION {
     static const int VALUE = a + b;
 };
 
+template <int a, int b> struct MAX {
+    static const int VALUE = a > b ? a : b;
+};
+
+template <int a, int b> struct MIN {
+    static const int VALUE = a < b ? a : b;
+};
+
 int main(int argc, char **argv) {
     std::cout << "head = " << myList::HEAD << std::endl;
     std::cout << "sum = " << myList::SUM << std::endl;
     std::cout << "minimum = " << myList::MINIMUM << std::endl;
-    std::cout << "map_reduce = " << MAP_REDUCE< myList, SQUARE, ADDITION, 0 >::RESULT << std::endl;
+    std::cout << "sum of squares = " << MAP_REDUCE< myList, SQUARE, ADDITION, 0 >::RESULT << std::endl;
+    // I wasn't able to pass MAX_INT from <climits> to this template?
+    std::cout << "minimum, map_reduce style = " << MAP_REDUCE< myList, ID, MIN, 32167 >::RESULT << std::endl;
+    std::cout << "sum, map_reduce style = " << MAP_REDUCE< myList, ID, ADDITION, 0 >::RESULT << std::endl;
 }
+    
