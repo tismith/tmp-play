fa7b42454b9b1d459eaa84e6be283c566a856a5a
Toby Smith <toby@tismith.id.au>
2011-07-13 07:02:35 +1000

    Tidy up the main routine

--- list.cpp
+++ list.cpp
@@ -2,7 +2,7 @@
 #include <climits>
 #include "list.hpp"
 
-#define myList LIST4<2,3,2,4>::TYPE
+#define myList LIST5<10,23,4,2,9>::TYPE
 
 template <int n> struct SQUARE {
     static const int VALUE = n*n;
@@ -33,32 +33,43 @@ template <int a, int b> struct ORD {
 };
 
 int main(int argc, char **argv) {
+
+    /* The best example */
     std::cout << "The list is: ";
     ListPrinter<myList>::print(); // this one generates tons of classes
+
+    std::cout << "The sorted list is: ";
+    ListPrinter<SORT<ORD, myList>::TYPE>::print();
+
+    /* basic list operations */
     std::cout << "head = " << myList::HEAD << std::endl;
     std::cout << "length = " << myList::LENGTH << std::endl;
     std::cout << "sum = " << myList::SUM << std::endl;
     std::cout << "minimum = " << myList::MINIMUM << std::endl;
-    std::cout << "sum of squares = " << MAP_REDUCE< myList, SQUARE, ADDITION, 0 >::RESULT << std::endl;
-    std::cout << "minimum, map_reduce style = " << MAP_REDUCE< myList, ID, MIN, INT_MAX >::RESULT << std::endl;
-    std::cout << "sum, map_reduce style = " << MAP_REDUCE< myList, ID, ADDITION, 0 >::RESULT << std::endl;
-    std::cout << "map square over the list = ";
+
+    /* map-reduce or fold */
+    std::cout << "sum of squares with map_reduce = " << MAP_REDUCE< myList, SQUARE, ADDITION, 0 >::RESULT << std::endl;
+    std::cout << "minimum with map_reduce = " << MAP_REDUCE< myList, ID, MIN, INT_MAX >::RESULT << std::endl;
+    std::cout << "sum done with map_reduce = " << MAP_REDUCE< myList, ID, ADDITION, 0 >::RESULT << std::endl;
+    std::cout << "mapping square over the list = ";
     ListPrinter<MAP<myList, SQUARE>::TYPE>::print();
-    std::cout << "map increment over the list = ";
+    std::cout << "mapping increment over the list = ";
     ListPrinter<MAP<myList, INCREMENT >::TYPE>::print();
+
+    /* other list operations */
     std::cout << "prepending '3' = ";
     ListPrinter<PREPEND<3,myList>::TYPE>::print();
-    std::cout << "tail myList = ";
+    std::cout << "tail function = ";
     ListPrinter<TAIL<myList>::TYPE>::print();
-    std::cout << "tail myList take 2 = ";
+    std::cout << "tail member function = ";
     ListPrinter<myList::TAIL>::print();
+
+    /* internal routines used for the merge sort */
     std::cout << "merge (1,6,8) (6,9) = ";
     ListPrinter<MERGE<ORD, LIST3<1,6,8>::TYPE, LIST2<6,9>::TYPE>::TYPE>::print();
     std::cout << "fst . split (1,6,8,6,9) = ";
     ListPrinter<SPLIT<LIST5<1,6,8,6,9>::TYPE>::TYPE::FST>::print();
     std::cout << "snd . split (1,6,8,6,9) = ";
     ListPrinter<SPLIT<LIST5<1,6,8,6,9>::TYPE>::TYPE::SND>::print();
-    std::cout << "sort (5,4,6,2,10) = ";
-    ListPrinter<SORT<ORD, LIST5<5,4,6,2,10>::TYPE>::TYPE>::print();
 }
     
