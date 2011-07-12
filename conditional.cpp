29eb804bd26c3def17591495d0c11b0a2851f190
Toby Smith <toby@tismith.id.au>
2011-07-12 20:00:55 +1000

    Better example for the IF

--- conditional.cpp
+++ conditional.cpp
@@ -9,7 +9,23 @@ struct B {
     static const int RESULT = 0;
 };
 
+struct A1 {
+    static inline int EXEC(void) {
+        std::cout << "TRUE!";
+    }
+};
+
+struct B1 {
+    static inline int EXEC(void) {
+        std::cout << "FALSE!";
+    }
+};
+
 int main(int argc, char **argv) {
     std::cout << "true = " << IF<true, A, B>::TEST::RESULT << std::endl;
     std::cout << "true = " << IF<false, A, B>::TEST::RESULT << std::endl;
+
+    std::cout << "true = ";
+    IF<true, A1, B1>::TEST::EXEC();
+    std::cout << std::endl;
 }
