1cf0bcf32bf8f4edad49d860c8b5548fc4f288eb
Toby Smith <toby@tismith.id.au>
2014-01-24 06:32:22 +1000

    Tidyups

--- conditional.cpp
+++ conditional.cpp
@@ -23,7 +23,7 @@ struct B1 {
 
 int main(int argc, char **argv) {
     std::cout << "true = " << IF<true, A, B>::TEST::RESULT << std::endl;
-    std::cout << "true = " << IF<false, A, B>::TEST::RESULT << std::endl;
+    std::cout << "false = " << IF<false, A, B>::TEST::RESULT << std::endl;
 
     std::cout << "true = ";
     IF<true, A1, B1>::TEST::EXEC();
